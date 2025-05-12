d81c8bf6a3c0c1fc10f49d9f15014cbf3d180900
roseycalamari <rosaruben7@gmail.com>
2025-05-12 03:03:35 +0100

    Remove sensitive data and add example config

--- public/js/config.js
+++
@@ -1,5 +0,0 @@
-// Supabase configuration
-const config = {
-    supabaseUrl: 'https://opbhmugrsxioweucrtiu.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wYmhtdWdyc3hpb3dldWNydGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMzA1MjcsImV4cCI6MjA2MDgwNjUyN30.2VyKbw7LPOChGxBu_aMT9Y4LXh2tC-tL1zCeEIZby-E'
-}; 
